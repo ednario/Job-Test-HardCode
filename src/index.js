3750159dcac3b86946318b9354bc35d773420226
Ednario Andrade <joseednario637@gmail.com>
2022-07-22 07:39:36 -0300

    express-handlebars incrementado e configurado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 const routes = require('./routes');
 
 const app = express();
 const port = 3000;
+
+// Config HandleBars
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
 
 routes(app);
 
